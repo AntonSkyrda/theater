426c569096ae7481a0c919e8092c3ab207306906
AntonSkyrda <mr.skyrda@gmail.com>
2024-05-30 00:50:18 +0300

    update models

--- theater/models.py
+++ theater/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class Actor(models.Model):
@@ -9,18 +10,22 @@ class Actor(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.second_name}"
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.second_name}"
+
     class Meta:
-        ordering = "second_name"
+        ordering = ["second_name"]
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering = "name"
+        ordering = ["name"]
 
 
 class Play(models.Model):
@@ -33,7 +38,7 @@ class Play(models.Model):
         return f"{self.title} {self.description}"
 
     class Meta:
-        ordering = "title"
+        ordering = ["title"]
 
 
 class TheaterHall(models.Model):
@@ -41,28 +46,30 @@ class TheaterHall(models.Model):
     rows = models.PositiveIntegerField()
     seats_in_row = models.PositiveIntegerField()
 
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
     def __str__(self):
         return f"{self.name}"
 
     class Meta:
-        ordering = "name"
+        ordering = ["name"]
 
 
 class Performance(models.Model):
     show_time = models.DateTimeField()
     play = models.ForeignKey(
         Play,
-        on_delete=models.CASCADE,
-        related_name="performances"
+        on_delete=models.CASCADE
     )
     theater_hall = models.ForeignKey(
         TheaterHall,
-        on_delete=models.CASCADE,
-        related_name="performances"
+        on_delete=models.CASCADE
     )
 
     def __str__(self):
-        return f"{self.show_time}"
+        return self.play.title + " " + str(self.show_time)
 
 
 class Reservation(models.Model):
@@ -75,6 +82,9 @@ class Reservation(models.Model):
 
     def __str__(self):
         return f"{self.created_at}"
+
+    class Meta:
+        ordering = ["-created_at"]
 
 
 class Ticket(models.Model):
@@ -91,5 +101,48 @@ class Ticket(models.Model):
         related_name="tickets"
     )
 
+    @staticmethod
+    def validate_ticket(row, seat, theater_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theater_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(theater_hall, theater_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                                          f"number must be in available range: "
+                                          f"(1, {theater_hall_attr_name}): "
+                                          f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theater_hall,
+            ValidationError,
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
     def __str__(self):
-        return f"{self.row} {self.seat}"
+        return (
+            f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
+        ordering = ["row", "seat"]
